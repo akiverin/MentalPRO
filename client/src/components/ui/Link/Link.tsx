c3699c3de0abfacdb94814c74ae38da6e74262cf
“akiverin” <kiverin03@yandex.ru>
2025-03-03 19:12:57 +0300

    add link component

---
+++ client/src/components/ui/Link/Link.tsx
@@ -0,0 +1,61 @@
+import { FC, AnchorHTMLAttributes } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import classNames from "classnames";
+import "./Link.scss";
+
+export type LinkBackground = "primary" | "secondary" | "light";
+export type LinkVariant = "default" | "rounded";
+
+interface TheLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  variant?: LinkVariant;
+  background?: LinkBackground;
+  fullWidth?: boolean;
+  small?: boolean;
+  className?: string;
+  disabled?: boolean;
+  to?: LinkProps["to"]; // This allows support for react-router's Link component
+}
+
+const TheLink: FC<TheLinkProps> = ({
+  children,
+  variant = "default",
+  background = "primary",
+  fullWidth = false,
+  className = "",
+  small = false,
+  disabled = false,
+  to = "#",
+  ...rest
+}) => {
+  const linkClasses = classNames(
+    "link",
+    `link--${variant}`,
+    `link--${background}`,
+    {
+      "link--full-width": fullWidth,
+    },
+    {
+      "link--disabled": disabled,
+    },
+    {
+      "link--small": small,
+    },
+    className
+  );
+
+  if (to) {
+    return (
+      <Link className={linkClasses} to={to} {...rest}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <a className={linkClasses} {...rest}>
+      {children}
+    </a>
+  );
+};
+
+export default TheLink;
