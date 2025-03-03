15dc1cea8060d519232d327fea463adff5a92b23
“akiverin” <kiverin03@yandex.ru>
2025-03-03 23:14:28 +0300

    partially add input component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import TheHeader from "./components/TheHeader/TheHeader";
+import Input from "./components/ui/Input/Input";
 import "./styles/main.scss";
 
 function App() {
   return (
     <>
       <TheHeader />
+      <Input placeholder="Email" fullWidth />
     </>
   );
 }
