110e9fe68b685df298b095b063d51ceb82168118
“akiverin” <kiverin03@yandex.ru>
2025-03-03 15:57:34 +0300

    created project, importing libraries, customizing scss styles and files, adding variables and structure

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Привет GGG</h1>
+      <a href="#">123</a>
+    </>
+  );
+}
+
+export default App;
