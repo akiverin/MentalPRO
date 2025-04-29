becb3382915b433862bd1293eb36553f6050e9b3
akiverin <kiverin03@yandex.ru>
2025-04-29 12:59:00 +0300

    upd branch

--- client/src/pages/Registration/Registration.tsx
+++ client/src/pages/Registration/Registration.tsx
@@ -63,70 +63,33 @@ const slides = [
 const Registration = () => {
   return (
     <div className="registration">
-      <div className="registration__wrapper">
-        <Slider slides={slides} slideDuration={10} />
-        <div className="registration__content">
-          <div className="registration__info">
-            <h1 className="registration__title">Регистрация</h1>
-            <p className="registration__desc">
-              У вас уже есть аккаунт?{" "}
-              <TheLink to="/login" variant="text">
-                Войти
-              </TheLink>
-            </p>
-            <Form className="registration__form" onSubmit={() => {}}>
-              <div className="registration__name-inputs">
-                <Input
-                  fullWidth
-                  placeholder="Имя"
-                  type="text"
-                  name="firstNameInput"
-                  required
-                />
-                <Input
-                  fullWidth
-                  placeholder="Фамилия"
-                  type="text"
-                  name="secondNameInput"
-                  required
-                />
-              </div>
+      <Slider slides={slides} slideDuration={10} />
+      <div className="registration__content">
+        <div className="registration__info">
+          <h1 className="registration__title">Регистрация</h1>
+          <p className="registration__desc">
+            У вас уже есть аккаунт?{" "}
+            <TheLink to="/login" variant="text">
+              Войти
+            </TheLink>
+          </p>
+          <Form className="registration__form" onSubmit={() => {}}>
+            <div className="registration__name-inputs">
               <Input
                 fullWidth
-                placeholder="Email"
-                type="email"
-                name="emailInput"
+                placeholder="Имя"
+                type="text"
+                name="firstNameInput"
                 required
               />
               <Input
-                placeholder="Пароль"
-                type="password"
-                name="passwordInput"
                 fullWidth
+                placeholder="Фамилия"
+                type="text"
+                name="secondNameInput"
                 required
               />
-              <Checkbox>
-                Даю{" "}
-                <TheLink variant="text" to="/presonal-data">
-                  согласие
-                </TheLink>{" "}
-                на обработку персональных данных
-              </Checkbox>
-              <div className="registration__actions">
-                <Button
-                  size="large"
-                  className="registration__button"
-                  fullWidth
-                  type="submit"
-                >
-                  Создать аккаунт
-                </Button>
-              </div>
-            </Form>
-            <div className="registration__extra">
-              <h2 className="registration__extra-title">Регистрация через</h2>
             </div>
-<<<<<<< HEAD
             <Input
               fullWidth
               placeholder="Email"
@@ -151,35 +114,13 @@ const Registration = () => {
             <div className="registration__actions">
               <Button
                 size="large"
-=======
-            <div className="registration__oauth">
-              <TheLink
-                variant="button"
-                background="secondary"
->>>>>>> d499fc5 (upd adaptive registration page and checkbox component)
                 className="registration__button"
+                fullWidth
+                type="submit"
               >
-                <img
-                  src={iconVKID}
-                  alt="VK ID icon"
-                  className="registration__icon"
-                />
-                <p className="visually-hidden">Использовать VK ID</p>
-              </TheLink>
-              <TheLink
-                variant="button"
-                background="secondary"
-                className="registration__button"
-              >
-                <img
-                  src={iconYandexID}
-                  alt="Яндекс ID icon"
-                  className="registration__icon"
-                />
-                <p className="visually-hidden">Использовать Яндекс ID</p>
-              </TheLink>
+                Создать аккаунт
+              </Button>
             </div>
-<<<<<<< HEAD
           </Form>
           <div className="registration__extra">
             <h2 className="registration__extra-title">Регистрация через</h2>
@@ -201,8 +142,6 @@ const Registration = () => {
               <IconYID />
               <p className="visually-hidden">Использовать Яндекс ID</p>
             </TheLink>
-=======
->>>>>>> d499fc5 (upd adaptive registration page and checkbox component)
           </div>
         </div>
       </div>
