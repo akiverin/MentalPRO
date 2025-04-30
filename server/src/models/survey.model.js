650afdde2544e36578c0d65c6ca15dee2572c117
akiverin <kiverin03@yandex.ru>
2025-04-30 19:08:19 +0300

    server: upd survey

--- server/src/models/survey.model.js
+++ server/src/models/survey.model.js
@@ -4,7 +4,9 @@ const surveySchema = new mongoose.Schema(
   {
     title: { type: String, required: true, maxlength: 255 },
     description: { type: String, required: true },
-    content: { type: String, required: true },
+    details: { type: String, required: true },
+    time: { type: Number, required: true },
+    results: { type: String, required: false },
     image: { type: String, required: false },
     isActive: { type: Boolean, default: true },
   },
